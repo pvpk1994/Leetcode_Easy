eccada933302445b7730ed990303b32c2b45ba63
Pavan Kumar Paluri <pvpk1994@gmail.com>
2020-09-25 00:05:04 -0500

    Find Difference using Bit Manipulation - XOR O(N)

--- Python/Find_Difference.py
+++ Python/Find_Difference.py
@@ -13,3 +13,10 @@ class Solution:
         for character in hash_map.keys():
             if hash_map[character]<0:
                 return character
+         
+        # Time Complexity: O(N)
+        result = 0
+	    for i in range(len(t)-1):
+		    result ^= ord(s[i])^ord(t[i])
+	    result = result^ord(t[-1])
+	    return chr(result)
